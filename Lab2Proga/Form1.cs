fcc60ee05d09f050542bd6c47e595f4c1b0cd6c5
Егор Павлов <egor12200@gmail.com>
2021-03-21 19:38:36 +0700

    3 л.р

--- Lab2Proga/Form1.cs
+++ Lab2Proga/Form1.cs
@@ -7,35 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection; //для позднего связывания
+//using FBuilder; //для раннего связывания
 
 namespace Lab2Proga
 {
     public partial class Form1 : Form
     {
+        //путь для позднего связывания
+        string dllPath = @"C:\Users\EgorP\source\repos\Egor-Pavlov\Lab2Proga\FBuilder\bin\Debug\netcoreapp3.1\FBuilder.dll";
+
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() != DialogResult.OK)
                 return;
 
-            MenuBulder menuBulder = new MenuBulder(openFileDialog1.FileName, this);
+            //позднее связывание
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+            Type type = assembly.GetType("FBuilder.Builder", true, true);
+            object obj = Activator.CreateInstance(type);
+            MethodInfo methodInfo = type.GetMethod("Init");
+            object res = methodInfo.Invoke(obj, new object[] { openFileDialog1.FileName, this });
+            //конец позднего связывание
+
+
+            //MenuBulder menuBulder = new MenuBulder(openFileDialog1.FileName, this);//вызов конструктора класса не через dll (2 лаб работа)
+
+
+            //Builder fbulder = new Builder(openFileDialog1.FileName, this);//раннее связывание
         }
 
-        public void FirstMethod(object sender, EventArgs e)
-        {
-            MessageBox.Show("Вызван метод 'FirstMethod'");
-        }
-        public void SecondMethod(object sender, EventArgs e)
-        {
-            MessageBox.Show("Вызван метод 'SecondMethod'");
-        }
-        public void ThirdMethod(object sender, EventArgs e)
-        {
-            MessageBox.Show("Вызван метод 'ThirdMethod'");
-        }
+        //для 2 л.р
+        //public void FirstMethod(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("Вызван метод 'FirstMethod'");
+        //}
+        //public void SecondMethod(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("Вызван метод 'SecondMethod'");
+        //}
+        //public void ThirdMethod(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("Вызван метод 'ThirdMethod'");
+        //}
     }
 }
